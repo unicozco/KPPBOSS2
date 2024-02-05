d5b0b1a8d8a615b40ffef8e73d19fc4090539055
Unicozco <zaheerkhan24012000@gmail.com>
2024-02-05 20:04:19 +0530

    event

--- swiper.js
+++ swiper.js
@@ -1,27 +1,112 @@
 var swiper = new Swiper(".mySwiper", {
-    direction: "vertical",
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+  direction: "vertical",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  mousewheel: true,
+  slidesPerView: 1,
+});
+
+var swiper2 = new Swiper(".mySwiper2", {
+  direction: "horizontal",
+  effect: "fade",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  slidesPerView: 1,
+  centeredSlides: true,
+  // loop: true,
+  autoplay: {
+    delay: 2500,
+    // disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+});
+
+var swiper3 = new Swiper(".mySwiper3", {
+  // slidesPerView: 4,
+  spaceBetween: 30,
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
     },
-    mousewheel: true,
-    slidesPerView: 1,
-  });
-  
-  var swiper2 = new Swiper(".mySwiper2", {
-    direction: "horizontal",
-    effect: "fade",
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
     },
-    slidesPerView: 1,
-    centeredSlides: true,
-    // loop: true,
-    autoplay: {
-      delay: 2500,
-      // disableOnInteraction: false,
-      pauseOnMouseEnter: true,
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 50,
     },
-  });
-  
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// events sliders
+
+var TrandingSlider = new Swiper(".tranding-slider", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var TrandingSlider = new Swiper(".tranding-slider1", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// navbar js
+const navbar = document.querySelector(".custom-header");
+const openBtn = document.querySelector(".navbar-open");
+
+openBtn.addEventListener("click", (event) => {
+  navbar.classList.toggle("open");
+});
+
+// Ubed JS for Nav Toggle
